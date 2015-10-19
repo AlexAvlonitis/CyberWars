24321b9e5bf5c59077e268c016f2f3fcbcb7a024
Alex <alex.avlonitis.xela@gmail.com>
2015-10-19 22:41:36 +0100

    thinking

--- CyberWars.rb
+++ CyberWars.rb
@@ -1,57 +1,71 @@
+require "./assets/modules/Commands.rb"
 
 
-class MainPC
-	require "./assets/modules/Commands.rb"
-	def enter()
-		puts "HackOS is booting up...\nplease wait"
+class PC
+
+	def initialize(pcname, name)
+		@pcname = pcname
+		@name = name
+	end
+
+	def enter
+		puts "#{@pcname} is booting up...\nplease wait"
 		sleep(3) #Add a booting up illusion
-		puts "Enter your name to start:"
-		@name = gets.chomp
 		puts "Welcome #{@name}"
+	end
+
+end
+
+class MyPC < PC
+
+	def initialize(pcname, name)
+		super(pcname,name)		
+	end
+
+	def subcommands
 		endLoop = 0 
 		while endLoop == 0
 			puts "Type: help, to view available commands"
-			print "root@HackOS:#/ "
+			print "root@#{@pcname}:#/ "
 			option = gets.chomp
 			case option
 				when "help" then Commands.help 
 				when "commands" then Commands.commands 
-			    when "emails" then Commands.emails 
+			    when "emails" then Commands.emails
+			    when "portscan server21.contoso.com" then Commands.portscan
+			    when "portscan" then puts "You haven't specified a host!"
 			    when "exit" then exit 
 			else
 				puts "Wrong input, try again"
 			end
 		end
 	end
+
 end
 
 class Missions
 	def enter()
-
+		
 	end
 end
 
 class Proxies
 	def enter()
 
-	end
-end
-
-class Emails
-	def enter()
 
 	end
 end
 
-class Targets
-	def enter()
-
+class Target < PC
+	def initialize(pcname, name)
+		super(pcname,name)		
 	end
 end
 
 
 
 
-start = MainPC.new
+mypc = MyPC.new(pcname="hackOS",name="Alex")
 
-start.enter
+mypc.enter
+mypc.subcommands
--- assets/modules/Commands.rb
+++ assets/modules/Commands.rb
@@ -6,6 +6,24 @@ module Commands
 	end	
 
 	def self.commands
-		puts "(1)To find the open ports of a remote host, \n   Type: portscan <hostname Or Ip of the target>, eg. portscan 32.134.12.3"
+		puts "(1)To find the open ports of a remote host,\nType: portscan <hostname Or Ip of the target>, eg. portscan 32.134.12.3"
+		puts "(2)To Brute Force a service, Type: bruteforce <hostname or IP> <port number>, eg. bruteforce 32.2.13.4 80"
+	end
+
+	def self.portscan
+		puts "Scanning..."
+		sleep(2)
+		puts "open ports:"
+		puts "-21 ftp"
+		puts "-80 http filtered"
+	end
+
+	def self.emails
+		puts "Welcome to CryptoMailer service. You have 1 new message"
+		puts "-From: Secret Client-31"
+		puts "-Subject: Need Cash?"
+		puts "-Body: Hello,\nI've heard you are one of the best in the field and i would need your help."
+		puts "The instructions are the following.\nI will need you to break into this server: server21.contoso.com,"
+		puts "find their client details and send them to me.\nI trust your skills and your payment will be 5000$."
 	end
 end
