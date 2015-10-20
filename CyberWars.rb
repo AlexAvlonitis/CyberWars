f56fe66287431ed70c7cff8a5be0e6343e2879b7
unknown <alex.avlonitis.xela@gmail.com>
2015-10-20 17:49:15 +0100

    almost done

--- CyberWars.rb
+++ CyberWars.rb
@@ -1,71 +1,147 @@
 require "./assets/modules/Commands.rb"
 
 
-class PC
 
-	def initialize(pcname, name)
+class PC
+	def enter()
+	    puts "This scene is not yet configured. Subclass it and implement enter()."
+	    exit(1)
+    end
+end
+
+class Engine
+  def initialize(scene_map)
+    @scene_map = scene_map
+  end
+
+  def play()
+    current_scene = @scene_map.opening_scene
+    last_scene = @scene_map.next_scene('completed')
+    while current_scene != last_scene
+	    next_scene_name = current_scene.enter
+	    current_scene = @scene_map.next_scene(next_scene_name)
+	end
+    current_scene.enter
+  end  
+end
+
+
+class MyPC < PC
+	def initialize(pcname="hackOS", name="Alex")
 		@pcname = pcname
 		@name = name
 	end
 
 	def enter
-		puts "#{@pcname} is booting up...\nplease wait"
-		sleep(3) #Add a booting up illusion
+		puts ""
+		puts "Connecting to #{@pcname}...\nplease wait"
+		sleep(2) #Add a loading illusion
+		puts ""
 		puts "Welcome #{@name}"
-	end
-
-end
-
-class MyPC < PC
-
-	def initialize(pcname, name)
-		super(pcname,name)		
-	end
-
-	def subcommands
+		puts ""
+		puts "-= Remember the three rules! Scan, Gain Access, Cover your tracks. =-"
 		endLoop = 0 
 		while endLoop == 0
 			puts "Type: help, to view available commands"
+			puts ""
 			print "root@#{@pcname}:#/ "
 			option = gets.chomp
 			case option
-				when "help" then Commands.help 
-				when "commands" then Commands.commands 
-			    when "emails" then Commands.emails
-			    when "portscan server21.contoso.com" then Commands.portscan
-			    when "portscan" then puts "You haven't specified a host!"
-			    when "exit" then exit 
+				when "help" then MyPcCommands.help 
+				when "commands" then MyPcCommands.commands 
+			    when "emails" then MyPcCommands.emails
+			    when "connect server21.contoso.com 21" then return 'target' ;
+			    when "connect server21.contoso.com 80" then puts "Can't connect, the remote host is either blocking our connection or a service is not running on that port";
+			    when "portscan server21.contoso.com 21" then MyPcCommands.portscan ;
+			    when "portscan server21.contoso.com 80" then puts "Scan failed, the remote host is either blocking our scan or a service is not running on that port" ;
+			    when "bruteforce server21.contoso.com 21" then MyPcCommands.portscan ;
+			    when "exit" then exit ;
 			else
 				puts "Wrong input, try again"
 			end
 		end
 	end
-
 end
 
-class Missions
+class Completed < PC
 	def enter()
-		
-	end
-end
-
-class Proxies
-	def enter()
-
-
+		true
 	end
 end
 
 class Target < PC
-	def initialize(pcname, name)
-		super(pcname,name)		
+
+	def initialize(pcname="server21", name="")
+		@pcname = pcname
+		@name = name
+	end
+
+	def enter
+		endLoop = 0 
+		while endLoop == 0
+			puts ""
+			puts "Connecting to #{@pcname}...\nplease wait"
+			sleep(2) #Add a loading illusion
+			puts ""
+			puts "Enter password to login:"
+			pass = gets.chomp
+			if pass == TargetCommands::PASSWORD
+				puts "Welcome Admin"
+				endLoop = 0 
+				while endLoop == 0
+					puts ""
+					puts "To list folders, Type: ls"
+					puts "To change folder, Type: cd <folder name>, eg. cd folder1"
+					puts "To disconnect from the host, Type: disconnect"
+					puts ""
+					print "root@#{@pcname}:#/ "
+					option = gets.chomp
+					case option
+						when "ls" then TargetCommands.ls ;
+						when "cd Myclients" then TargetCommands.cd ;
+					    when "cd " then puts "The folder name is case sensitive";
+					    when "disconnect" then return 'mypc' ;
+					else
+						puts ""
+						puts "Wrong input, try again"
+					end
+				end
+			else
+				puts ""
+				puts "Wrong password. disconnecting..."
+				return 'mypc'
+			end
+		end
+	end
+end
+
+class Map
+	@@scenes = {
+		'mypc' => MyPC.new,
+		'target' => Target.new,
+		'completed' => Completed.new
+
+	}
+
+	def initialize(place)
+		@place = place
+	end
+
+	def next_scene(nextscene)
+		@@scenes[nextscene]
+	end
+
+	def opening_scene
+		return next_scene(@place)
 	end
 end
 
 
 
 
-mypc = MyPC.new(pcname="hackOS",name="Alex")
 
-mypc.enter
-mypc.subcommands
+newmap = Map.new('mypc')
+
+newgame = Engine.new(newmap)
+
+newgame.play
--- assets/modules/Commands.rb
+++ assets/modules/Commands.rb
@@ -1,29 +1,66 @@
-module Commands
+module MyPcCommands
 	def self.help
+		puts ""
 		puts "(1) To see all the available commands, type: commands"
 		puts "(2) To view your emails, type: emails"
 		puts "(3) To exit, type: exit"
+		puts ""
 	end	
 
 	def self.commands
+		puts ""
 		puts "(1)To find the open ports of a remote host,\nType: portscan <hostname Or Ip of the target>, eg. portscan 32.134.12.3"
-		puts "(2)To Brute Force a service, Type: bruteforce <hostname or IP> <port number>, eg. bruteforce 32.2.13.4 80"
+		puts ""
+		puts "(2)To connect to a remote server,\nType: connect <hostname or IP> <port number>, eg. connect 32.2.13.4 1234"
+		puts ""
+		puts "(3)To Brute Force a service,\nType: bruteforce <hostname or IP> <port number>, eg. bruteforce 32.2.13.4 1234"
+		puts ""
 	end
 
 	def self.portscan
+		puts ""
 		puts "Scanning..."
 		sleep(2)
 		puts "open ports:"
 		puts "-21 ftp"
 		puts "-80 http filtered"
+		puts ""
+	end
+
+	def self.bruteforce
+		puts ""
+		puts "Bruteforcing {@pcname} 21, this might take a while..."
+		puts "#{rand(10)}"
 	end
 
 	def self.emails
-		puts "Welcome to CryptoMailer service. You have 1 new message"
+		puts ""
+		puts "-=Welcome to CryptoMailer service. You have a new message=-"
+		puts ""
 		puts "-From: Secret Client-31"
 		puts "-Subject: Need Cash?"
 		puts "-Body: Hello,\nI've heard you are one of the best in the field and i would need your help."
-		puts "The instructions are the following.\nI will need you to break into this server: server21.contoso.com,"
+		puts "I will need you to break into this server: server21.contoso.com,"
 		puts "find their client details and send them to me.\nI trust your skills and your payment will be 5000$."
+		puts ""
 	end
+end
+
+module TargetCommands
+
+	def self.ls
+		puts ""
+		puts "Name:Myclients    Type:Folder     Size:5k   Date_Modified:#{Time.now} "
+		puts ""
+	end	
+
+	def self.cd 
+		puts ""
+		puts "clients"
+		puts ""
+	end
+
+	PASSWORD = 'noaccess'
+
+	
 end
